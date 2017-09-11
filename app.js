07833a533542a32ce89063805f85c33f192c33b8
pierscr2 <31768018+pierscr2@users.noreply.github.com>
2017-09-11 23:31:29 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,4 +3,6 @@ var http = require('http')
 http.createServer((req,res) => {
   console.log("waiting");
   res.end('hola');
-}).listen(8080);
+}).listen(8080,'127.0.0.1',, function(){
+  console.log('Io aspetto eh');
+}););
