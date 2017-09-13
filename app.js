36eb3ea162346a7acfcfc1112e98ca124c17ec76
Tizio Incognito <pierscr82@gmail.com>
2017-09-13 23:10:16 +0200

    express!

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 var http = require('http')
 var port = process.env.PORT||process.env.OPENSHIFT_NODEJS_PORT || 8080, ip=process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
+var express = require('express');
+var app = express();
 
-http.createServer((req,res) => {
-  console.log("waiting");
-  res.end('hola');
-}).listen(port,ip, function(){
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+
+app.listen(port,ip, function(){
   console.log('Io aspetto eh');
   console.log('la porta è :'+port);
   console.log('ip :'+ip);
