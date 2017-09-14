6ddf053a7dd19b7d72e5d462029d767b68852bf8
piero <piero.scrima@arancia-ict.it>
2017-09-14 10:27:11 +0200

    miaapp

--- app.js
+++ app.js
@@ -1,29 +1,56 @@
-//  OpenShift sample Node application
-var express = require('express'),app     = express();
-
+var http=require('http');
+var console=require('console');
+var so_api=require('os');
+var url=require('url');
+var filesys = require("fs");
+var path=require('path');
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
+function newHtmlLine(string){
+  return '<div>'+string+'</div>';
+}
 
-app.get('/', function (req, res) {
-  res.end("mah");
+http.createServer((req,res) => {
+   var h=req.headers;
+   var ext=path.extname(url);
+   var urlParts = url.parse(req.url, true);
+
+
+   if(ext!=''){
+      //ritorna qualcosa
+      res.write('<html><body>');
+     console.log(h);
+     var urlParts = url.parse(req.url, true);
+     for(k in h){
+
+         res.write(newHtmlLine(k+' -> '+h[k]));
+     }
+     for(var i=0;i<10;i++){
+         res.write('')
+     }
+
+     res.write("url----> "+h.host+req.url);
+     console.log(urlParts);
+     res.write(newHtmlLine(urlParts.href));
+     res.end('</body></html>');
+   }else{
+     var fullpath = __dirname+urlParts.path;
+     filesys.readFile(fullpath,(err,data)=>{
+       if(err) {
+         res.writeHeader(404, {"Content-Type": "text/plain"});
+         res.write('not found');
+         res.end();
+       }else{
+         res.writeHeader(200);
+         res.write(data);
+         res.end();
+      }
+     })
+   }
+
+}).listen(port, ip, function(){
+  console.log('la porta : '+port);
+  console.log('l\'ip: '+ ip);
 });
-
-app.get('/pagecount', function (req, res) {
-  res.end('non è bello cio che bello, ma che bello che blelo che bello');
-});
-
-// error handling
-app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.status(500).send('Something bad happened!');
-});
-
-
-app.listen(port, ip,function(){
-    console.log('Server running on http://%s:%s', ip, port);
-});
-
-
-module.exports = app ;
