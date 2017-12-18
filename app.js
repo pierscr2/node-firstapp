15f849ee700109117a2b8c67e0db624d2972eff5
Tizio Incognito <pierscr82@gmail.com>
2017-12-18 22:53:09 +0100

    bug fix

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var so_api=require('os');
 var url=require('url');
 var filesys = require("fs");
 var path=require('path');
-var process=require('Process');
+var process=require('process');
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8090,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
